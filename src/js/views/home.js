b15395b68048ee3ddd5b975afaf1a23ef98d2e39
bonh0303 <fbonhomme9721@gmail.com>
2022-07-06 01:55:01 +0000

    Updated Input and Delete functionality

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,32 +2,41 @@
 import React from "react";
 import "../../styles/home.css";
 import { useContext, createContext, useState } from "react";
+import { Context } from "../store/appContext";
+import Inputs from "./Inputs.js";
 
-const Context = createContext();
 
 export const Home = () => {
-  // Initialize array with placeholder values
-  const tasksList = ["test1", "test2", "test3"];
+  //Set Context
+  const { store, actions } = useContext(Context);
 
-  //Set States
-  const [newTask, setnewTask] = useState(tasksList);
-  const [remainingTasks, setremainingTasks] = useState(newTask.length);
+  //Initialize States
+  const [textEntered, setTextEntered] = useState("");
+  const [tasks, setTasks] = useState(store.list);
+  
 
-  //Append new task to current array
-  const handleNewTask = (newTask) => {
-    setnewTask((current) => [...current, newTask]);
-    setremainingTasks(remainingTasks + 1);
-  };
+  //Update Input value
+  function inputValue(e) {
+    const itemValue = e.target.value;
+    setTextEntered(itemValue);
+  }
 
-  //Remove "li" when X button is clicked
-  function handleDeleteTask(item) {
-    item.target.parentNode.parentNode.remove();
-    setremainingTasks(remainingTasks - 1);
+  //Add a new task
+  function addNewTask(e) {
+    if (e.key === "Enter") {
+      setTasks(actions.todoList(textEntered))
+      setTextEntered("");
+    }
+  }
+
+  //Delete task by id value
+  function deleteTask(id) {
+    setTasks(actions.deleteTask(id))
   }
 
   return (
     <Context.Provider
-      value={{ newTask, remainingTasks, handleDeleteTask, handleNewTask }}
+      value={{textEntered, tasks, deleteTask, addNewTask, inputValue }}
     >
       <ToDos />
     </Context.Provider>
@@ -37,63 +46,39 @@ export const Home = () => {
 const ToDos = () => {
   const value = useContext(Context);
 
-  const listItems = value.newTask.map((item, index) => {
-    return (
-      <li key={index} className="d-flex flex-row justify-spacing-between">
-        {item}
-        <span
-          type="button"
-          onClick={value.handleDeleteTask}
-          className="delete-button"
-        >
-          <i className="fas fa-times"></i>
-        </span>
-      </li>
-    );
-  });
-
   return (
     <div>
       <h1 className="todo-header">Todos</h1>
       <div className="todos-container d-flex flex-column">
         <div className="todos-container-header d-flex flex-row">
           <span className="me-3">Tasks</span>
-          <Inputs onTask={value.handleNewTask} />
+          <input
+            type="text"
+            onChange={value.inputValue}
+            onKeyDown={value.addNewTask}
+            value={value.textEntered}
+          />
         </div>
 
         <div className="todos-container-body flex-grow-1">
-          <ul>{listItems}</ul>
+          <ul>
+            {value.tasks.map((task, index) => (
+              <Inputs
+                key={index}
+                id={index}
+                task={task}
+                onDelete={value.deleteTask}
+              />
+            ))}
+          </ul>
         </div>
 
-        <div className="todos-container-footer flex-grow-1">
-          {value.remainingTasks === 0
-            ? "No tasks, add a task"
-            : `Number of Tasks: ${value.remainingTasks}`}
+        <div className="flex-grow-1">
+           {value.tasks.length === 0
+        ? "No tasks, add a task"
+        : `Number of Tasks: ${value.tasks.length}`}
         </div>
       </div>
     </div>
-  );
-};
-
-const Inputs = (props) => {
-  const [inputValue, setInputValue] = useState("");
-
-  const targetInputEvent = (e) => {
-    setInputValue(e.target.value);
-  };
-  const passToParent = (e) => {
-    if (e.key === "Enter") {
-      props.onTask(inputValue);
-      setInputValue("");
-    }
-  };
-  return (
-    <input
-      type="text"
-      onChange={targetInputEvent}
-      value={inputValue}
-      onKeyDown={passToParent}
-      placeholder="Enter Task"
-    />
   );
 };
